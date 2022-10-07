194ee7cc873cd747cebc15eeb6558246f70f6b9d
白条君 <zs199990309@163.com>
2022-10-07 21:19:47 +0800

    main first committed

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @类名 Test
+ * @描述 TODO
+ * @作者 白条君
+ * @创建日期 2022/10/7 21:17
+ * @版本 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
